efb31b7defd367d4c188f582799f102d51853fa4
FaturFawkes <nur.faturohman28@gmail.com>
2022-10-13 21:57:14 +0700

    add 2 endpoint

--- main.go
+++ main.go
@@ -11,5 +11,11 @@ func main() {
 	e.GET("/hello", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "hello world")
 	})
+	e.GET("/fatur", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hello fatur")
+	})
+	e.GET("/home", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "ini halaman home")
+	})
 	e.Start(":8000")
 }
